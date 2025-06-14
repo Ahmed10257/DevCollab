1afd90bd8462d7d1851fdd6abdb2062e02d6d9c6
Ahmed10257 <ahmed.mansour10257@gmail.com>
2025-06-14 10:59:00 +0300

    fixing naming convention

--- back-end/src/user/user.service.ts
+++ back-end/src/user/user.service.ts
@@ -1,4 +1,3 @@
-import { users } from './../dirzzle/schema/user.schema';
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
