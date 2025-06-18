af54cb26f78fe83ee76ab59bd79f59d13dd03289
Ahmed10257 <ahmed.mansour10257@gmail.com>
2025-06-18 14:46:26 +0300

    modifying development configurations

--- back-end/drizzle.config.ts
+++ back-end/drizzle.config.ts
@@ -3,8 +3,10 @@ import 'dotenv/config';
 // This file is used to configure drizzle-kit for generating the database schema
 // and types. It is not used in the production code.
 
+const isDev = process.env.NODE_ENV === 'development';
+
 export default defineConfig({
-    schema: './dist/drizzle/schema/**.ts',
+    schema: isDev ? './src/drizzle/schema/**.ts' : './dist/drizzle/schema/**.js',
     out: './drizzle',
     dialect: 'postgresql',
     dbCredentials: {
